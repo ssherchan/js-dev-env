419399f29edad0b31c5f0f61cae700057ec1fc26
Ravi Chelikani <Ravi.Chelikani@chrobinson.com>
2017-03-02 09:27:22 -0600

    latest changes

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,27 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 function get(url) {
-  return fetch(url).then(onSucess, onError);
+  return fetch(baseUrl + url).then(onSucess, onError);
+}
+
+// Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSucess, onError);
 }
 
 function onSucess(response) {
